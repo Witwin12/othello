168bc3d2b77de143265729ff2b3b71717095b4d1
Witwin12 <kunwin12@gmail.com>
2023-10-16 19:34:56 +0700

    แก้

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import board as b
 import tkinter as tk
-
+def play():
+    board = b.Board(root)
+    board.place(x=50, y=50)
 root = tk.Tk()
 root.title('OTHELLO !')
 root.geometry('1000x700')
-
-board = b.Board(root)
-board.place(x=50, y=50)
-
+play_button = tk.Button(root,text='Play',command=play)
+play_button.pack()
 root.mainloop()
