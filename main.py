da84a18f9402db5fed2011b106a01b97e342ec55
Witwin12 <kunwin12@gmail.com>
2023-10-16 19:43:50 +0700

    แก้

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import board as b
 import tkinter as tk
 def play():
     board = b.Board(root)
-    board.place(x=50, y=50)
+    board.place(x=50, y=60)
 root = tk.Tk()
 root.title('OTHELLO !')
 root.geometry('1000x700')
-play_button = tk.Button(root,text='Play',command=play)
-play_button.pack()
+title = tk.Label(root,text="OTHELLO",font=('arial',40))
+title.pack()
+play_button = tk.Button(root,text='Player VS player',command=play,width=10,height=5)
+play_button.pack(padx=100,pady=100)
 root.mainloop()
--- title.py
+++
@@ -1,9 +0,0 @@
-import main as m
-import tkinter as tk
-root = tk.Tk()
-root.title('OTHELLO !')
-root.geometry('1000x700')
-play_button = tk.Button(text='Play')
-play_button.bind("<ButtonPress-1>",m)
-play_button.pack()
-root.mainloop()
