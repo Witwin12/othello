0c29c73c8fed63db139524e561c392e2199bcec5
Witwin12 <kunwin12@gmail.com>
2023-10-17 21:19:17 +0700

    แก้

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ def player_vs_player():
     board = b.Board(root)
     board.place(x=50, y=50)
     board.configure(bg= 'light blue')
-
 root = tk.Tk()
 root.title('OTHELLO !')
 root.geometry('1000x700')
 root.configure(bg='light blue')
 pvp_button = tk.Button(root,text='Player VS player',command=player_vs_player,width=20,height=5,bg='green')
 pvp_button.pack(padx=200,pady=100)
-
 root.mainloop()
